4f8269d5c20c7a12894bedb0ebb0803a2754b3a8
sattyam <sattyamthakur02@gmail.com>
2022-04-21 22:41:34 +0530

    Changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#e3f2fd" }}>
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/all">AllEvents</Link>
+                        <Link className="navbar-brand" to="/all" style={{fontWeight:"bold"}}>ALLEVENTS</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
